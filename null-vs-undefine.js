acd7fe72927e1d4f4618575f727f6ab380762edc
saidur1529 <saidur.rahman04@northsouth.edu>
2021-02-02 17:23:04 +0600

    null vs undefine cleared for interview

---
+++ null-vs-undefine.js
@@ -0,0 +1,16 @@
+undefine javascript
+let pakhi;
+function add(num1, num2){
+    console.log(num1+num2);
+    return;
+}
+function add(num1, num2){
+    console.log(num2);
+}
+const result = add(13);
+const premik = {name:"smart dude", phone: 45221211};
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
+let ages = [2, 5, 9, 11];
+console.log(ages[11]);
